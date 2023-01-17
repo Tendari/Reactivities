a5f7c18bb2e7dd31743384d8f71c5018dfbb8d43
artem.roi <artem.roi@globallogic.com>
2023-01-17 07:49:54 +0200

    Added react router and navigation

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
-import App from './app/layout/App';
+import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { router } from './app/router/Routex';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 
 );
